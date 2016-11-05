ea3f3406646e62d7a01c24a231230ea3f5206af2
Josh Davey <jdavey1996@gmail.com>
2016-11-05 22:17:40 +0000

    Theme update & TweetActivity redesign
    The app theme has been updated as well as the design of TweetActivity -
    this is a now a popup activity. Included in the updates is a text hint
    and a clear all 'x' button for the tweet edittext. A camera icon from
    Google material icons has been added to load the camera intent.
    Additionally, a textview has been placed to the right of this icon that
    states whether an image has been linked to the tweet. When no image is
    added it displays 'No image added' but once an image has been captured,
    it displays 'Image added - click to remove', and is made clickable to
    remove the image from the tweet.

--- app/src/main/java/com/josh_davey/mobile_computing_assignment/TweetActivity.java
+++ app/src/main/java/com/josh_davey/mobile_computing_assignment/TweetActivity.java
@@ -10,9 +10,12 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.webkit.CookieManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.twitter.sdk.android.core.Callback;
@@ -24,6 +27,7 @@ import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterAuthClient;
 import com.twitter.sdk.android.core.internal.TwitterApi;
+import com.twitter.sdk.android.core.models.Image;
 import com.twitter.sdk.android.core.models.Media;
 import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.core.services.MediaService;
@@ -44,33 +48,32 @@ import retrofit2.http.POST;
 import retrofit2.http.Part;
 
 public class TweetActivity extends Activity {
-
+    //Variables.
     TwitterAuthClient mTwitterAuthClient;
-    Uri uri;
+    Uri savedImageUri;
+    EditText tweetInput;
+    TextView captureImgStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
+
+        //Sets the activity size - makes a popup window. http://stackoverflow.com/questions/16007750/how-to-create-custom-popupwindow-from-intent-call
+        getWindow().setLayout(LinearLayoutCompat.LayoutParams.MATCH_PARENT, LinearLayoutCompat.LayoutParams.WRAP_CONTENT);
+
+        tweetInput = (EditText) findViewById(R.id.tweetInput);
+        //Gets instance of captureImgStatus edittext and sets it to unclickable
+        captureImgStatus = (TextView) findViewById(R.id.captureImgStatus);
+        captureImgStatus.setClickable(false);
     }
 
     public void startTwitter(View view)
     {
         mTwitterAuthClient = new TwitterAuthClient();
-        Tweets twitter = new Tweets(this,this,mTwitterAuthClient);
+        Tweets twitter = new Tweets(this,this,mTwitterAuthClient,savedImageUri,tweetInput);
         twitter.checkActiveSession();
     }
-
-    //Clears session, logs user out and removes all cookies - removes traces of user login left.
-    public void logoutTwitter(View view)
-    {
-        TwitterCore.getInstance().getSessionManager().clearActiveSession();
-        TwitterCore.getInstance().logOut();
-        //http://stackoverflow.com/questions/29743676/how-to-logout-from-twitter-using-fabric-sdk-for-android
-        CookieManager.getInstance().removeAllCookies(null);
-        CookieManager.getInstance().flush();
-    }
-
 
     @Override
     protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
@@ -78,16 +81,25 @@ public class TweetActivity extends Activity {
         if(requestCode == TwitterAuthConfig.DEFAULT_AUTH_REQUEST_CODE) {
             mTwitterAuthClient.onActivityResult(requestCode, responseCode, intent);
         }
-        else if(requestCode == 1 && responseCode == RESULT_OK) {
-            Toast.makeText(TweetActivity.this, "saved image", Toast.LENGTH_SHORT).show();
+        else if(requestCode == 1) {
+            if(responseCode == RESULT_OK) {
+                Toast.makeText(TweetActivity.this, "saved image", Toast.LENGTH_SHORT).show();
+                captureImgStatus.setText("Image added - click to remove");
+                captureImgStatus.setClickable(true);
 
-            //startTwitter(null);
+            }
+            else
+            {
+                //Error saving captured image.
+                //Removed the saved uri - image didn't get saved.
+                savedImageUri = null;
 
+            }
         }
     }
 
-    //Take photo intent.
-    public void dispatchTakePictureIntent(View view) {
+    //Take photo intent - https://developer.android.com/training/camera/photobasics.html.
+    public void captureImg(View view) {
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -99,9 +111,11 @@ public class TweetActivity extends Activity {
                 outputDir.mkdirs();
 
                 File newFile = new File(path + "/" + "latestImgToTweet.png");
-                uri = Uri.fromFile(newFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-                startActivityForResult(takePictureIntent, 1);
+
+                savedImageUri = Uri.fromFile(newFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,savedImageUri);
+                startActivityForResult(takePictureIntent,1);
             }
             else
             {
@@ -121,7 +135,7 @@ public class TweetActivity extends Activity {
         switch (requestCode) {
             case 0:
                 if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    dispatchTakePictureIntent(null);
+                    captureImg(null);
                 }
                 else
                 {
@@ -131,50 +145,37 @@ public class TweetActivity extends Activity {
         }
     }
 
-/*****THIS METHOD IS STILL BEING TESTED - uploads captured image and if successful, tweets it.*/
-    //https://docs.fabric.io/android/twitter/access-rest-api.html
-    //http://stackoverflow.com/questions/31785698/android-adding-image-to-tweet-using-fabric-twitter-rest-api-and-retrofit
-    //*************https://dev.twitter.com/rest/reference/post/statuses/update CHECK THIS OUT....*******
-    //Used for request body initialising Retroyfit 2.0- http://stackoverflow.com/questions/34562950/post-multipart-form-data-using-retrofit-2-0-including-image
-    public void tweetStatusTest(View view) {
-        //Get apiclient instance that's authenticated already.
-        final TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
-
-        //Get image taken.
-        File photo = new File(uri.getPath());
-
-        //Create request body with image to upload.
-        RequestBody file = RequestBody.create(MediaType.parse("image/*"), photo);
-
-        //Upload image, request callback.
-        MediaService ms = twitterApiClient.getMediaService();
-        Call<Media> uploadImgCall = ms.upload(file, null, null);
-        uploadImgCall.enqueue(new Callback<Media>() {
-            @Override
-            public void success(Result<Media> result) {
-                Toast.makeText(TweetActivity.this, "uploaded" + result.data.mediaIdString, Toast.LENGTH_SHORT).show();
-                //If successfully, create tweet with user text entry and mediaId of image previously uploaded.
-                EditText text = (EditText) findViewById(R.id.tweetTxt);
-                StatusesService statusesService = twitterApiClient.getStatusesService();
-                Call<Tweet> tweetCall = statusesService.update(text.getText().toString(), null, false, null, null, null, false, false, result.data.mediaIdString);
-                tweetCall.enqueue(new Callback<Tweet>() {
-                    @Override
-                    public void success(Result<Tweet> result) {
-                        //Do something with result
-                        Toast.makeText(TweetActivity.this, "Tweeted", Toast.LENGTH_SHORT).show();
-                    }
-
-                    public void failure(TwitterException exception) {
-                        //Do something on failure
-                        Toast.makeText(TweetActivity.this, "Not Tweeted", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-
-            @Override
-            public void failure(TwitterException exception) {
-                Toast.makeText(TweetActivity.this, "not uploaded" + exception, Toast.LENGTH_SHORT).show();
-            }
-        });
+    //Used to clear tweet input edittext when 'x' to the right of it is pressed
+    public void clearInput(View view)
+    {
+        tweetInput.setText("");
     }
+
+    //Clears the saved image uri varaible when the remove text is pressed. This prevents the captured image from being tweeted.
+    public void clearImage(View view)
+    {
+        if (captureImgStatus.getText().toString() == "Image added - click to remove")
+        {
+            //Removing uri from varaible so no is attached to the tweet.
+            savedImageUri = null;
+            //Setting captureImgStatus text.
+            captureImgStatus.setText("No image added");
+            //Disables clicking.
+            captureImgStatus.setClickable(false);
+        }
+    }
+
+
+
+    /*ADD THIS METHOD ELSEWHERE - Possibly toolbar
+    //Clears session, logs user out and removes all cookies - removes traces of user login left.
+    public void logoutTwitter(View view)
+    {
+        TwitterCore.getInstance().getSessionManager().clearActiveSession();
+        TwitterCore.getInstance().logOut();
+        //http://stackoverflow.com/questions/29743676/how-to-logout-from-twitter-using-fabric-sdk-for-android
+        CookieManager.getInstance().removeAllCookies(null);
+        CookieManager.getInstance().flush();
+    }*/
+
 }
--- app/src/main/java/com/josh_davey/mobile_computing_assignment/Tweets.java
+++ app/src/main/java/com/josh_davey/mobile_computing_assignment/Tweets.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.twitter.sdk.android.core.Callback;
@@ -24,6 +26,7 @@ import java.io.File;
 
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
+import retrofit2.Call;
 
 ////http://stackoverflow.com/questions/27267809/using-custom-login-button-with-twitter-fabric
 //*****************8https://docs.fabric.io/android/twitter/compose-tweets.html#results*** USED FOR ALL TWITTER STUFF
@@ -31,12 +34,17 @@ public class Tweets {
     Context ctx;
     Activity activity;
     TwitterAuthClient mTwitterAuthClient;
+    TwitterApiClient twitterApiClient;
+    Uri savedImageUri;
+    EditText tweetInput;
 
-    public Tweets(Context ctx, Activity activity, TwitterAuthClient mTwitterAuthClient)
+    public Tweets(Context ctx, Activity activity, TwitterAuthClient mTwitterAuthClient, Uri savedImageUri, EditText tweetInput)
     {
         this.ctx = ctx;
         this.activity = activity;
         this.mTwitterAuthClient = mTwitterAuthClient;
+        this.savedImageUri = savedImageUri;
+        this.tweetInput = tweetInput;
     }
 
     public void checkActiveSession()
@@ -47,7 +55,13 @@ public class Tweets {
         }
         else
         {
-            createTweet();
+            if(savedImageUri == null) {
+                sendTweet(null);
+            }
+            else
+            {
+                uploadMedia(savedImageUri);
+            }
         }
     }
 
@@ -57,7 +71,14 @@ public class Tweets {
 
             @Override
             public void success(Result<TwitterSession> twitterSessionResult) {
-                createTweet();
+                Toast.makeText(ctx, twitterSessionResult.data.getUserName(), Toast.LENGTH_SHORT).show();
+                if(savedImageUri == null) {
+                    sendTweet(null);
+                }
+                else
+                {
+                    uploadMedia(savedImageUri);
+                }
             }
 
             @Override
@@ -68,19 +89,56 @@ public class Tweets {
         });
     }
 
+    public void uploadMedia(Uri uri) {
+        //Get apiclient instance that's authenticated already.
+        twitterApiClient = TwitterCore.getInstance().getApiClient();
 
-    public void createTweet()
-    {
-        final TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
-        final Intent intent = new ComposerActivity.Builder(ctx)
-                .session(session)
-                .createIntent();
-        ctx.startActivity(intent);
+        //Get image taken.
+        File photo = new File(uri.getPath());
+
+        //Create request body with image to upload.
+        RequestBody file = RequestBody.create(MediaType.parse("image/*"), photo);
+
+        //Upload image, request callback.
+        MediaService ms = twitterApiClient.getMediaService();
+        Call<Media> uploadImgCall = ms.upload(file, null, null);
+        uploadImgCall.enqueue(new Callback<Media>() {
+            @Override
+            public void success(Result<Media> result) {
+                Toast.makeText(ctx, "Image uploaded" + result.data.mediaIdString, Toast.LENGTH_SHORT).show();
+                sendTweet(result.data.mediaIdString);
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(ctx, "Image not uploaded" + exception, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
 
-
-
-
-
+    public void sendTweet(String mediaIdString)
+    {
+        /*****THIS METHOD IS STILL BEING TESTED - uploads captured image and if successful, tweets it.*/
+        //https://docs.fabric.io/android/twitter/access-rest-api.html
+        //http://stackoverflow.com/questions/31785698/android-adding-image-to-tweet-using-fabric-twitter-rest-api-and-retrofit
+        //*************https://dev.twitter.com/rest/reference/post/statuses/update CHECK THIS OUT....*******
+        //Used for request body initialising Retroyfit 2.0- http://stackoverflow.com/questions/34562950/post-multipart-form-data-using-retrofit-2-0-including-image
+        twitterApiClient = TwitterCore.getInstance().getApiClient();
+        StatusesService statusesService = twitterApiClient.getStatusesService();
+        Call<Tweet> tweetCall = statusesService.update(tweetInput.getText().toString(), null, false, null, null, null, false, false, mediaIdString);
+        tweetCall.enqueue(new Callback<Tweet>() {
+            @Override
+            public void success(Result<Tweet> result) {
+                //Do something with result
+                Toast.makeText(ctx, "Tweeted", Toast.LENGTH_SHORT).show();
+                activity.finish();
+            }
+            public void failure(TwitterException exception) {
+                //Do something on failure
+                Toast.makeText(ctx, "Not Tweeted", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
